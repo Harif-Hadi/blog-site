9062b800470d7802a2bc1dc2289ba046d3628978
Harif-Hadi <hadi4harif@gmail.com>
2023-12-26 13:31:52 +0000

    prepared for deployment

---
+++ components/posts/post-detail/post-content.js
@@ -0,0 +1,75 @@
+import ReactMarkdown from "react-markdown";
+import Image from "next/image";
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import atomDark from "react-syntax-highlighter/dist/cjs/styles/prism/atom-dark";
+import js from "react-syntax-highlighter/dist/cjs/languages/prism/javascript";
+import css from "react-syntax-highlighter/dist/cjs/languages/prism/css";
+
+import PostHeader from "./post-header";
+import classes from "./post-content.module.css";
+
+SyntaxHighlighter.registerLanguage("js", js);
+SyntaxHighlighter.registerLanguage("css", css);
+
+function PostContent(props) {
+  const { post } = props;
+
+  const imagePath = `/posts/${post.slug}/${post.image}`;
+
+  const customRenderers = {
+    p(paragraph) {
+      const { node } = paragraph;
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/posts/${post.slug}/${image.properties.src}`}
+              alt={image.properties.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+      return <p>{paragraph.children}</p>;
+    },
+    code(props) {
+      const { children, className, node, ...rest } = props;
+      const match = /language-(\w+)/.exec(className || "");
+      return match ? (
+        <SyntaxHighlighter
+          {...rest}
+          PreTag="div"
+          children={String(children).replace(/\n$/, "")}
+          language={match[1]}
+          style={atomDark}
+        />
+      ) : (
+        <code {...rest} className={className}>
+          {children}
+        </code>
+      );
+    },
+
+    image(image) {
+      return (
+        <Image
+          src={`/posts/${post.slug}/${image.url}`}
+          alt={image.alt}
+          width={600}
+          height={300}
+        />
+      );
+    },
+  };
+
+  return (
+    <article className={classes.content}>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
+    </article>
+  );
+}
+
+export default PostContent;
---
+++ next.config.js
@@ -0,0 +1,36 @@
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {
+        mongodb_username: "hadi4harif",
+        mongodb_password: "KTRQ75clYpRcsjkA",
+        mongodb_cluster_name: "cluster0",
+        mongodb_database_name: "my-site-dev",
+      },
+    };
+  }
+
+  return {
+    env: {
+      mongodb_username: "hadi4harif",
+      mongodb_password: "KTRQ75clYpRcsjkA",
+      mongodb_cluster_name: "cluster0",
+      mongodb_database_name: "my-site",
+    },
+    future: {
+      webpack5: true, // by default, if you customize webpack config, they switch back to version 4.
+      // Looks like backward compatibility approach.
+    },
+    webpack(config) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
+        // by next.js will be dropped. Doesn't make much sense, but how it is
+        fs: false, // the solution
+      };
+
+      return config;
+    },
+  };
+};
