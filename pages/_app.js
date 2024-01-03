6c6b0fe31536b2f301775486c85b938a1929b212
Harif-Hadi <hadi4harif@gmail.com>
2024-01-03 22:38:34 +0000

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 
 import "../styles/globals.css";
 import Layout from "../components/layout/layout";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -9,6 +10,18 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-FS7CYVZPJG"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-FS7CYVZPJG');
+    `}
+      </Script>
       <Component {...pageProps} />
     </Layout>
   );
